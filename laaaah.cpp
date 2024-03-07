5b8a4f229096410449345165fd4c7eea47fecc22
tegarramadhannn <161449736+tegarramadhannn@users.noreply.github.com>
2024-03-07 17:29:03 +0700

    variabel ketiga

--- laaaah.cpp
+++ laaaah.cpp
@@ -28,6 +28,25 @@ void input() {
 
 }
 
+void bubbleSortArray() { //procedur untuk mengurutkan array dengan metode bublle sort
+    
+   int pass = 1; // step 1
+
+   do {
+       for (int j = 0; j <= n - 1 - pass; j++) { // step 2
+           if (arr[j] > arr[j + 1]) { //step 3
+               int temp = arr[j];
+               arr[j] = arr[j + 1];
+               arr[j + 1] = temp;
+
+
+           }
+       }
+       pass = pass + 1; //step 4
+
+   } while (pass <= n); //step 5
+}
+
 
    
 int main()
