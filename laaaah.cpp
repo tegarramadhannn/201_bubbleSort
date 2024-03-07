7d7e1c39272b3c994d830d6742a8cbcdc5886921
tegarramadhannn <161449736+tegarramadhannn@users.noreply.github.com>
2024-03-07 17:26:36 +0700

    variabel kedua

--- laaaah.cpp
+++ laaaah.cpp
@@ -4,6 +4,30 @@ using namespace std;
 int arr[20];
 int n;
 
+void input() {
+    while (true) {
+        cout << "masukan banyaknya elemen pada array : ";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else {
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+
+        }
+    }
+    cout << endl;
+    cout << "===============" << endl;
+    cout << "masukan Elemen Array" << endl;
+    cout << "===============" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+
+    }
+
+}
+
 
    
 int main()
