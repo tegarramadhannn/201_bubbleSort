f1e596901ada0133f966d2f1fe748739ea67eab6
tegarramadhannn <161449736+tegarramadhannn@users.noreply.github.com>
2024-03-07 17:34:42 +0700

    terakhir

--- laaaah.cpp
+++ laaaah.cpp
@@ -60,8 +60,14 @@ void display() {
     cout << endl;
 }
 
-   
 int main()
 {
-    
+    input();  //memanggil prosedur untuk menginput data
+    bubbleSortArray();  //memanggil prosedur bubblesort untuk proses mengurutkan data
+    display();//memanggil prosedur untuk menampilkan data yang telah urut
+    system("pause");
+
+
+    return 0;
 }
+   
